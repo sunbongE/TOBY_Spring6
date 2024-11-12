679e294945f0148308477382b1d977361bf6b014
박태호 <qkrxogh7@naver.com>
2024-11-12 22:54:00 +0900

    관심사 분리 : 메서드 추출로 환율정보를 가져오는 로직을 분리했다.

--- src/main/java/tobyspring/hellospring/PaymentService.java
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -13,28 +13,24 @@ import java.util.stream.Collectors;
 
 public class PaymentService {
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
-        // 환율 가져오기
-        // https://open.er-api.com/v6/latest/USD
-        // Alt+F12 -> http -v "URL입력"
+        BigDecimal exRate = getExRate(currency);
+        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
+        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
+
+        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
+    }
+
+    private BigDecimal getExRate(String currency) throws IOException {
         URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         String response = br.lines().collect(Collectors.joining());
         br.close();
 
-//        System.out.println(response);
         ObjectMapper mapper = new ObjectMapper();
         ExRateData data = mapper.readValue(response, ExRateData.class);
         BigDecimal exRate = data.rates().get("KRW"); // 환율 꺼내오기.
-
-        // 금액 가져오기 : foreignCurrencyAmount * exRate -> BigDecimal에서는 multiply사용
-        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
-
-        // 유효 시간 계산
-        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
-
-        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
-
+        return exRate;
     }
 
     public static void main(String[] args) throws IOException {
