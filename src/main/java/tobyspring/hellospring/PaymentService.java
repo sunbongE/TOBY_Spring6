f50afa0c1831ea96b8c44127f465973f6c105928
박태호 <qkrxogh7@naver.com>
2024-11-14 19:33:50 +0900

    코드 정리

--- src/main/java/tobyspring/hellospring/PaymentService.java
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -1,15 +1,8 @@
 package tobyspring.hellospring;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.time.LocalDateTime;
-import java.util.stream.Collectors;
 
 abstract public class PaymentService {
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
