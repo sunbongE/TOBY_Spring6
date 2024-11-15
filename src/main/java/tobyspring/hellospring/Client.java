182587f744eb6a04ec2f8e3b8d0b099017c509e4
박태호 <qkrxogh7@naver.com>
2024-11-15 15:12:40 +0900

    오브젝트팩토리 적용 및 학습내용정리

--- src/main/java/tobyspring/hellospring/Client.java
+++ src/main/java/tobyspring/hellospring/Client.java
@@ -5,11 +5,10 @@ import java.math.BigDecimal;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        PaymentService paymentService = new PaymentService(new WebApiExRateProvider());
-        Payment payment = paymentService.prepare(100L, "KRW", BigDecimal.valueOf(50.7));
-        PaymentService paymentService2 = new PaymentService(new SimpleExRateProvider());
-        Payment payment2 = paymentService2.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+        ObjectFactory objectFactory = new ObjectFactory();
+        PaymentService paymentService = objectFactory.paymentService();
+
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
-        System.out.println(payment2);
     }
 }
