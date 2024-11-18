c98f4c28dbb4d1deb426a29132dc2400d7790b0e
Taeho Park <qkrxogh7@gmail.com>
2024-11-18 17:24:32 +0900

    Test Stub 적용한 테스트

--- src/test/java/tobyspring/hellospring/payment/PaymentServiceTest.java
+++ src/test/java/tobyspring/hellospring/payment/PaymentServiceTest.java
@@ -3,33 +3,36 @@ package tobyspring.hellospring.payment;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import tobyspring.hellospring.exrate.WebApiExRateProvider;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class PaymentServiceTest {
 
     @Test
     @DisplayName("prepare 메소드가 요구사항 3가지를 충족했는지")
     void prepare() throws IOException {
-        PaymentService paymentService = new PaymentService(new WebApiExRateProvider());
+
+        testAmount(BigDecimal.valueOf(500), BigDecimal.valueOf(5_000));
+        testAmount(BigDecimal.valueOf(1000), BigDecimal.valueOf(10000));
+        testAmount(BigDecimal.valueOf(3000), BigDecimal.valueOf(30000));
+
+        // 원화환산금액 유효시간 계산
+//        Assertions.assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
+
+    }
+
+    private static void testAmount(BigDecimal exRate, BigDecimal convertedAmount) throws IOException {
+        PaymentService paymentService = new PaymentService(new ExRateProviderStub(exRate));
 
         Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
 
         // 환율정보가져오기.
-        Assertions.assertThat(payment.getExRate()).isNotNull();
+        Assertions.assertThat(payment.getExRate()).isEqualByComparingTo(exRate);
 
         // 원화환산금액 계산
         Assertions.assertThat(payment.getConvertedAmount())
-                .isEqualTo(payment.getExRate().multiply(payment.getForeignCurrencyAmount()));
-
-        // 원화환산금액 유효시간 계산
-        Assertions.assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
-
+                .isEqualByComparingTo(convertedAmount);
     }
 
 }
