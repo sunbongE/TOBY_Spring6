63bf5c0f651c90590db1f928ad5ccbd34415d1cb
Taeho Park <qkrxogh7@gmail.com>
2024-11-18 15:39:32 +0900

    Test Code 2 : @BeforeEach적용

--- src/test/java/tobyspring/hellospring/SortTest.java
+++ src/test/java/tobyspring/hellospring/SortTest.java
@@ -8,16 +8,15 @@ import java.util.Arrays;
 import java.util.List;
 
 public class SortTest {
+    Sort sort;
 
     @BeforeEach // 각 테스트를 실핼할 때 실행
     void beforeEach(){
-
+        sort = new Sort();
     }
 
     @Test
     void sort(){
-        // 준비 given
-        Sort sort = new Sort();
 
         // 실행 when
         List<String> list = sort.sortByLength(Arrays.asList("aa", "b"));
@@ -28,8 +27,6 @@ public class SortTest {
     }
     @Test
     void sort3Items(){
-        // 준비 given
-        Sort sort = new Sort();
 
         // 실행 when
         List<String> list = sort.sortByLength(Arrays.asList("aa", "b","ccc"));
@@ -40,8 +37,6 @@ public class SortTest {
     }
     @Test
     void sortAlreadySorted(){
-        // 준비 given
-        Sort sort = new Sort();
 
         // 실행 when
         List<String> list = sort.sortByLength(Arrays.asList("b","aa","ccc"));
