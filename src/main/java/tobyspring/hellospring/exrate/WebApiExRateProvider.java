a1689fb07efc886855c00df89b771c2ab2813e1b
Taeho Park <qkrxogh7@gmail.com>
2024-11-22 22:14:08 +0900

    HttpClientApi사용해서 환율정보 가져오는 코드 추가.

--- src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
+++ src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
@@ -4,6 +4,9 @@ import tobyspring.hellospring.api.*;
 import tobyspring.hellospring.payment.ExRateProvider;
 
 import java.math.BigDecimal;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class WebApiExRateProvider implements ExRateProvider {
     ApiTemplate apiTemplate = new ApiTemplate();
@@ -11,7 +14,7 @@ public class WebApiExRateProvider implements ExRateProvider {
     public BigDecimal getExRate(String currency) {
         String url = "https://open.er-api.com/v6/latest/"+currency;
 
-        return apiTemplate.getExRate(url, new SimpleApiExecutor(), new ErApiExRateExtractor());
+        return apiTemplate.getExRate(url,new HttpClientApiExercutor() , new ErApiExRateExtractor());
     }
 
 
