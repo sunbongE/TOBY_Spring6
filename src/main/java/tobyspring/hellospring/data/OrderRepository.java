29af9dc94c2a0633a56bf134fc330a28e2b47d29
Taeho Park <qkrxogh7@gmail.com>
2024-11-25 23:34:58 +0900

    DataAccessException 추상화 예외처리 적용

--- src/main/java/tobyspring/hellospring/data/OrderRepository.java
+++ src/main/java/tobyspring/hellospring/data/OrderRepository.java
@@ -3,37 +3,17 @@ package tobyspring.hellospring.data;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.PersistenceContext;
 import tobyspring.hellospring.order.Order;
 
 import java.math.BigDecimal;
 
 public class OrderRepository {
 
-    private final EntityManagerFactory emf;
-
-    public OrderRepository(EntityManagerFactory emf) {
-        this.emf = emf;
-    }
+   @PersistenceContext
+   private EntityManager entityManager;
 
     public void save(Order order) {
-        // em
-        EntityManager em = emf.createEntityManager();
-        // transaction
-        EntityTransaction transaction = em.getTransaction();
-        transaction.begin();
-
-        try {
-            // em.persist
-            em.persist(order);// db에 넣기
-            em.flush();
-
-            transaction.commit();
-        }catch (RuntimeException e){
-            if(transaction.isActive()) transaction.rollback();
-            throw e;
-        }finally {
-            if(em.isOpen()) em.close();
-        }
-
+        entityManager.persist(order);// db에 넣기
     }
 }
--- src/main/java/tobyspring/hellospring/payment/DataClient.java
+++ src/main/java/tobyspring/hellospring/payment/DataClient.java
@@ -2,8 +2,13 @@ package tobyspring.hellospring.payment;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import org.hibernate.Transaction;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 import tobyspring.hellospring.DataConfig;
 import tobyspring.hellospring.data.OrderRepository;
 import tobyspring.hellospring.order.Order;
@@ -14,12 +19,27 @@ public class DataClient {
     public static void main(String[] args) {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
         OrderRepository repository = beanFactory.getBean(OrderRepository.class);
+        JpaTransactionManager transactionManager= beanFactory.getBean(JpaTransactionManager.class);
 
-        Order order = new Order("100", BigDecimal.TEN);
-        repository.save(order);
+        try{
 
-        System.out.println(order);
-        Order order2 = new Order("100", BigDecimal.ONE);
-        repository.save(order2);
+        new TransactionTemplate(transactionManager).execute( status -> {
+            Order order = new Order("100", BigDecimal.TEN);
+            repository.save(order);
+
+            System.out.println(order);
+            Order order2 = new Order("100", BigDecimal.ONE);
+            repository.save(order2);
+            return null;
+        });
+        }catch (DataIntegrityViolationException e){
+            System.out.println("주문번호 중복 복구 작업");
+
+        }
+
+
+
+
+        // commit
     }
 }
