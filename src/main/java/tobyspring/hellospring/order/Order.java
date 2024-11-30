6777d69e6fdc9de48feb1d2e3cdb011c46784519
Taeho Park <qkrxogh7@gmail.com>
2024-11-30 09:28:15 +0900

    기술에 독립적인 애플리케이션 서비스

--- src/main/java/tobyspring/hellospring/order/Order.java
+++ src/main/java/tobyspring/hellospring/order/Order.java
@@ -4,12 +4,8 @@ import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 
-@Entity
-@Table(name = "orders")
 public class Order {
-    @Id @GeneratedValue
     private Long id;
-    @Column(unique = true)
     private String no;
     private BigDecimal total;
 
